5c0915ad57ccdc95e76c05a60a1051fcf20e84d4
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-30 19:23:33 +0530

    Added - Bet Stake and bet value

--- src/com/example/gamblesimulator/GambleSimulator.java
+++ src/com/example/gamblesimulator/GambleSimulator.java
@@ -1,7 +1,11 @@
 package com.example.gamblesimulator;
 
 public class GambleSimulator {
+    public static  int everyDayStake = 100;
+    public static  int eachGameBet = 1;
     public static void main(String[] args) {
-        System.out.println("Welcome to Gamble simulator");
+        System.out.println("Every day stake: " + everyDayStake + "$");
+        System.out.println("Each game bet: " + eachGameBet + "$");
+
     }
 }
