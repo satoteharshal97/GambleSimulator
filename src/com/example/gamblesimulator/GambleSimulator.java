ee262fa9280b1b9473e3092fe2aa2050292f3d69
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-30 19:14:33 +0530

    Initial commit

---
+++ src/com/example/gamblesimulator/GambleSimulator.java
@@ -0,0 +1,7 @@
+package com.example.gamblesimulator;
+
+public class GambleSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gamble simulator");
+    }
+}
