c8201c87883a59092c1e810ade2e8fe9604981d3
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-30 19:27:51 +0530

    Added - Wither win or lose

--- src/com/example/gamblesimulator/GambleSimulator.java
+++ src/com/example/gamblesimulator/GambleSimulator.java
@@ -3,9 +3,21 @@ package com.example.gamblesimulator;
 public class GambleSimulator {
     public static  int everyDayStake = 100;
     public static  int eachGameBet = 1;
-    public static void main(String[] args) {
-        System.out.println("Every day stake: " + everyDayStake + "$");
-        System.out.println("Each game bet: " + eachGameBet + "$");
+    public static final int WON_GAME = 1;
+    public static final int LOST_GAME = 0;
 
+    public static void main(String[] args) {
+        gamble();
+    }
+    static void gamble(){
+        int bet = (int) Math.floor(Math.random() * 2);
+        switch(bet){
+            case WON_GAME:
+                System.out.println("Player won :" + (eachGameBet) + "$");
+                break;
+            case LOST_GAME:
+                System.out.println("Player lost :" + (eachGameBet) + "$");
+                break;
+        }
     }
 }
